258b07928c9efe7ce7b2e19f274ffba7a330d7cb
Bhambhwani, Jaikumar (US - Mumbai) <jbhambhwani@deloitte.com>
2014-06-30 17:05:40 +0530

    Initial Commit

---
+++ DesignableView/DesignableButton.swift
@@ -0,0 +1,64 @@
+//
+//  DesignableButton.swift
+//  IBDesignableDemoApp
+//
+//  Created by Bhambhwani, Jaikumar (US - Mumbai) on 6/30/14.
+//  Copyright (c) 2014 Jaikumar. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableButton: UIButton {
+
+    @IBInspectable
+    var fillColor : UIColor = UIColor.redColor() {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable
+    var strokeColor : UIColor = UIColor.blackColor() {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable
+    var strokeWidth : CGFloat = 0 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable
+    var cornerRadius : CGFloat = 10 {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        drawButtonWithFrame(rect, cornerRadius: self.cornerRadius, strokeWidth: self.strokeWidth, fillColor: self.fillColor, strokeColor: self.strokeColor)
+    }
+    
+    func drawButtonWithFrame(frame: CGRect, cornerRadius: CGFloat, strokeWidth: CGFloat, fillColor: UIColor, strokeColor: UIColor) {
+        
+        //// Rectangle Drawing
+        let rectanglePath = UIBezierPath(roundedRect: CGRectMake(frame.minX + 2, frame.minY + 2, floor((frame.width - 2) * 0.97297 + 0.5), floor((frame.height - 2) * 0.85714 + 0.5)), cornerRadius: cornerRadius)
+        fillColor.setFill()
+        rectanglePath.fill()
+        strokeColor.setStroke()
+        rectanglePath.lineWidth = strokeWidth
+        rectanglePath.stroke()
+    }
+
+}
---
+++ DesignableView/DesignableView.h
@@ -0,0 +1,19 @@
+//
+//  DesignableView.h
+//  DesignableView
+//
+//  Created by Bhambhwani, Jaikumar (US - Mumbai) on 6/30/14.
+//  Copyright (c) 2014 Jaikumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DesignableView.
+FOUNDATION_EXPORT double DesignableViewVersionNumber;
+
+//! Project version string for DesignableView.
+FOUNDATION_EXPORT const unsigned char DesignableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignableView/PublicHeader.h>
+
+
